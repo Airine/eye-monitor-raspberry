cfa89fdc3adcc84170ce81a9028534219666d76f
Aaron <Airine0119@gmail.com>
2019-03-22 11:46:44 +0800

    change tcp refer

--- TCP-mac-client.py
+++ TCP-mac-client.py
@@ -1,29 +1,89 @@
-from tcpcom import TCPClient
-import time
+from threading import Thread
+import socket, time
 
-IP_ADDRESS = "10.20.43.94"
+VERBOSE = False
+IP_ADDRESS = "192.168.0.17"
 IP_PORT = 22000
 
-def onStateChanged(state, msg):
-    global isConnected
-    if state == "CONNECTING":
-       print("Client:-- Waiting for connection...")
-    elif state == "CONNECTED":
-       print("Client:-- Connection estabished.")
-    elif state == "DISCONNECTED":
-       print("Client:-- Connection lost.")
-       isConnected = False
-    elif state == "MESSAGE":
-       print("Client:-- Received data:", msg)
+def debug(text):
+    if VERBOSE:
+        print "Debug:---", text
 
-client = TCPClient(IP_ADDRESS, IP_PORT, stateChanged = onStateChanged)
-rc = client.connect()
-if rc:
+# ------------------------- class Receiver ---------------------------
+class Receiver(Thread):
+    def run(self):
+        debug("Receiver thread started")
+        while True:
+            try:
+                rxData = self.readServerData()
+            except:
+                debug("Exception in Receiver.run()")
+                isReceiverRunning = False
+                closeConnection()
+                break
+        debug("Receiver thread terminated")
+
+    def readServerData(self):
+        debug("Calling readResponse")
+        bufSize = 4096
+        data = ""
+        while data[-1:] != "\0": # reply with end-of-message indicator
+            try:
+                blk = sock.recv(bufSize)
+                if blk != None:
+                    debug("Received data block from server, len: " + \
+                        str(len(blk)))
+                else:
+                    debug("sock.recv() returned with None")
+            except:
+                raise Exception("Exception from blocking sock.recv()")
+            data += blk
+        print "Data received:", data
+# ------------------------ End of Receiver ---------------------
+
+def startReceiver():
+    debug("Starting Receiver thread")
+    receiver = Receiver()
+    receiver.start()
+
+def sendCommand(cmd):
+    debug("sendCommand() with cmd = " + cmd)
+    try:
+        # append \0 as end-of-message indicator
+        sock.sendall(cmd + "\0")
+    except:
+        debug("Exception in sendCommand()")
+        closeConnection()
+
+def closeConnection():
+    global isConnected
+    debug("Closing socket")
+    sock.close()
+    isConnected = False
+
+def connect():
+    global sock
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    debug("Connecting...")
+    try:
+        sock.connect((IP_ADDRESS, IP_PORT))
+    except:
+        debug("Connection failed.")
+        return False
+    startReceiver()
+    return True
+
+sock = None
+isConnected = False
+
+if connect():
     isConnected = True
+    print "Connection established"
+    time.sleep(1)
     while isConnected:
-        print("Client:-- Sending command: go...")
-        client.sendMessage("go")
+        print "Sending command: go..."
+        sendCommand("go")
         time.sleep(2)
-    print("Done")
 else:
-    print("Client:-- Connection failed")
+    print "Connection to %s:%d failed" % (IP_ADDRESS, IP_PORT)
+print "done"
