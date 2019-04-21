44e44e457b8db23ceb81bae25d13968b1473b440
Aaron <Airine0119@gmail.com>
2019-04-22 02:49:33 +0800

    final

--- play_and_record.py
+++ play_and_record.py
@@ -3,13 +3,12 @@ import subprocess
 import time
 import argparse
 
-SAMPLE_RATE = 48000
-CHANNELS = 8
 AUDIO_NAME = 'raw_data/sig1822k_5s.wav'
+RECORD_NAME= ''
 PLAY_TIME = 5 # seconds
 
 # process_queue = Queue()
-# TODO: adding 
+# TODO: adding force termination.
 def play(audio, end_time=20.0):
     proc = subprocess.Popen(['aplay', '-d', str(end_time), audio])
     print(proc.pid)
@@ -25,12 +24,44 @@ def get_time_stamp():
     data_secs = (ct - int(ct)) * 1000
     return "%s-%03d" % (data_head, data_secs)
 
+def get_wave_duration(file_name):
+    with contextlib.closing(wave.open(file_name,'r')) as f:
+        frames = f.getnframes()
+        rate = f.getframerate()
+        duration = frames / float(rate)
+        return duration
+
 def main():
-    parser = argparse.ArgumentParser()
-    # parser.add_argument
-    file_name = get_time_stamp() + '-record.wav'
+    parser = argparse.ArgumentParser(description='play a audio and record in the same time')
+    # parser.add_argument('-h', '--help', )
+    # group = parser.add_mutually_exclusive_group()
+    parser.add_argument('-t', '--test', help='test with 5 seconds wav',
+                        action='store_true')
+    parser.add_argument('-d', '--duration', help='specify the play and record duration')
+    parser.add_argument('play_audio', type=str, default='raw_data/sig1822k_5s.wav', help='the file name of the audio needed to play.')
+    parser.add_argument('record_file', type=str, default='record/test.wav', help='the file path of your record')
+    args = parser.parse_args()
+
+    AUDIO_NAME = args.play_audio
+    RECORD_NAME= args.record_file
+
+    if args.t:
+        print('testing')
+        RECORD_NAME = get_time_stamp() + '-record.wav'
+        AUDIO_NAME = 'raw_data/sig1822k_5s.wav'
+
+    PLAY_TIME = get_wave_duration(AUDIO_NAME)
+
+    if args.d:
+        print('set duration')
+        if args.d < PLAY_TIME
+            PLAY_TIME = args.d
+            print('set success')
+        else:
+            print('set failed')
+
     play_p = Process(target=play, args=(AUDIO_NAME, PLAY_TIME,))
-    record_p = Process(target=record, args=(file_name, PLAY_TIME,))
+    record_p = Process(target=record, args=(RECORD_NAME, PLAY_TIME,))
     record_p.start()
     play_p.start()
     # start = time.time()
